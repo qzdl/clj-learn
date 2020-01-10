ee386a452423e1105e03be22b0f2292972b80af2
qzdl <accounts@samuelculpepper.com>
2020-01-10 21:40:02 +0000

    Add pong project, basics of cljs

--- hello-cljs/src/hello_world/core.cljs
+++ hello-cljs/src/hello_world/core.cljs
@@ -1,15 +1,40 @@
 (ns hello-world.core
-  (:require react-dom))
+  (:require [react-dom :as rd]
+            [cljs.core.async :as async
+              :refer [<! >! chan put! timeout]]
+            [clojure.string :as string]))
 
 (println "Hello world!")
 
 (defn average  [a b]
   (/ (+ a b) 2.0))
 
-(.render js/ReactDOM
-  (.createElement js/React "h2" nil "Hello, React!")
-  (.getElementById js/document "app"))
+(defn ex-0
+  (.render js/ReactDOM
+    (.createElement js/React "h1" nil "Hello, React!")
+    (.getElementById js/document "app")))
 
 ; (.render js/ReactDOM
 ;   (.createElement js/React "h2" nil "Hello, React!")
 ;   (.getElementById js/document "app"))
+
+(defn chanout [t c]
+  "returns ")
+(comment
+  (def c (chan))
+)
+
+(comment
+  (defn render [q]
+    ""
+    (apply str
+           (for [p (reverse q)]
+             (str "<div class=proc-" p "'>Process " p "</div>"))))
+  )
+
+(comment
+
+  (go (while true (<! (timeout 250)) (>! c 1)))
+  (go (while true (<! (timeout 1000)) (>! c 1)))
+  (go (while true (<! (timeout 2000)) (>! c 1)))
+)
